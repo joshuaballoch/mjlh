0669c946a6b6ffb9d501e1d5d20175681eb5c4dc
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-02 22:11:24 +0800

    move most pages into basic cards layout

--- masthead.php
+++ masthead.php
@@ -1,50 +1,66 @@
 <?php
-
       require_once "header.php";
-      
-      $runonce = true;
-      $displayyear = 0;
+?>
 
-      $query = "SELECT content_id FROM CONTENT where content_id like 'masthead%' order by content_id desc";
-      $result = mysql_query($query, GetMyConnection()) or die('Error getting masthead content: ' . mysql_error());
-      ?>
-      <h1 style="text-align: center;"><?php talk("Masthead ","Comit&eacute; de r&eacute;daction ",$lang); ?>
-      <SELECT name="mastheadselect" SIZE="1" onchange="window.open(this.options[this.selectedIndex].value,'_top')">
+<div class="row">
+  <div class="col-sm-8">
+    <div class="card">
       <?php
-      while ($line = mysql_fetch_array($result))
-      {
-         $year = substr($line[0],8);
-         $year += 2000;
-         $prevyear = $year - 1;
-         
-         if ($runonce)
-         {
-           $runonce = false;
-           $displayyear = $year;    //Display the most recent year by default unless overridden below with yr paramter.
-         }
+        $runonce = true;
+        $displayyear = 0;
 
-         if ($_REQUEST['yr'] == $year)
-         {
-           echo "<option selected = 'selected' ";
-         }
-         else
-         {
-           echo "<option ";
-         }
-         echo "value = 'masthead.php?yr=" . $year . "'>" .$prevyear . "-" .$year. "</option>\n";
-      }
-      echo "</SELECT></h1>";
-      
-     if (array_key_exists('yr', $_REQUEST))
-     {
-       $displayyear = $_REQUEST['yr'];
-     }
-     $displayyear = clean_input($displayyear);
-     $query = "select ".$lang." from CONTENT where content_id = 'masthead".substr($displayyear,2,2)."'";
-     $result = mysql_query($query, GetMyConnection()) or die('Error getting contents: ' . mysql_error());
-     $line = mysql_fetch_array($result);
-     echo $line[0];
+        $query = "SELECT content_id FROM CONTENT where content_id like 'masthead%' order by content_id desc";
+        $result = mysql_query($query, GetMyConnection()) or die('Error getting masthead content: ' . mysql_error());
+        ?>
+        <h1><?php talk("Masthead ","Comit&eacute; de r&eacute;daction ",$lang); ?>
+        <SELECT name="mastheadselect" SIZE="1" onchange="window.open(this.options[this.selectedIndex].value,'_top')">
+        <?php
+          while ($line = mysql_fetch_array($result)) {
+           $year = substr($line[0],8);
+           $year += 2000;
+           $prevyear = $year - 1;
 
+           if ($runonce)
+           {
+             $runonce = false;
+             $displayyear = $year;    //Display the most recent year by default unless overridden below with yr paramter.
+           }
 
-      require_once "footer.php";
+           if ($_REQUEST['yr'] == $year)
+           {
+             echo "<option selected = 'selected' ";
+           }
+           else
+           {
+             echo "<option ";
+           }
+           echo "value = 'masthead.php?yr=" . $year . "'>" .$prevyear . "-" .$year. "</option>\n";
+          }
+          echo "</SELECT></h1>";
+
+      ?>
+    </div>
+    <div class="card">
+      <?php
+        if (array_key_exists('yr', $_REQUEST)) {
+           $displayyear = $_REQUEST['yr'];
+         }
+         $displayyear = clean_input($displayyear);
+         $query = "select ".$lang." from CONTENT where content_id = 'masthead".substr($displayyear,2,2)."'";
+         $result = mysql_query($query, GetMyConnection()) or die('Error getting contents: ' . mysql_error());
+         $line = mysql_fetch_array($result);
+         echo $line[0];
+      ?>
+      <div class="card-share">
+        <?php require $_SERVER["DOCUMENT_ROOT"]."/components/shared/share.php" ?>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <?php require $_SERVER["DOCUMENT_ROOT"]."/components/shared/sidebar.php"; ?>
+  </div>
+</div>
+
+<?php
+  require_once "footer.php";
 ?>
--- page.php
+++ page.php
@@ -1,14 +1,29 @@
 <?php
-
       require_once "header.php";
+?>
 
-      if (array_key_exists('id', $_GET))
-      {
-         $query = "select ".$lang." from CONTENT where content_id =".clean_input($_GET['id']);
-         $result = mysql_query($query, GetMyConnection()) or die('Error getting contents: ' . mysql_error());
-         $line = mysql_fetch_array($result);
-         echo $line[0];
-      }
+<div class="row">
+  <div class="col-sm-8">
+    <div class="card">
+      <?php
+        if (array_key_exists('id', $_GET))
+        {
+           $query = "select ".$lang." from CONTENT where content_id =".clean_input($_GET['id']);
+           $result = mysql_query($query, GetMyConnection()) or die('Error getting contents: ' . mysql_error());
+           $line = mysql_fetch_array($result);
+           echo $line[0];
+        }
+      ?>
+      <div class="card-share">
+        <?php require $_SERVER["DOCUMENT_ROOT"]."/components/shared/share.php" ?>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <?php require $_SERVER["DOCUMENT_ROOT"]."/components/shared/sidebar.php"; ?>
+  </div>
+</div>
 
-      require_once "footer.php";
+<?php
+  require_once "footer.php";
 ?>
