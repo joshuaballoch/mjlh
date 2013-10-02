86bf9fa35d5da8394008b58d55334d30e745b9ba
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-02 16:17:31 +0800

    link fix

--- components/blog/post.php
+++ components/blog/post.php
@@ -1,7 +1,7 @@
 <?php
   echo "<div class=\"card\">";
   $read_more_link = "blog.php?blog_id=". $line['BLOG_ID'];
-  echo "<h2><a href =". $read_more_link ."'>" . $line['TITLE'] . "</a></h2><div style = \"padding-bottom: 5px; border-bottom: 1px dotted black;\">\n";
+  echo "<h2><a href =". $read_more_link .">" . $line['TITLE'] . "</a></h2><div style = \"padding-bottom: 5px; border-bottom: 1px dotted black;\">\n";
   talk ("Posted By","Post&eacute; par",$lang);
   echo " <strong>" . $line['AUTHOR'] . "</strong> <i>(" . $line['date'] . ")</i></div>\n";
   echo "<div id = 'homeblogentry'>" . printTruncated(800,$line['ENTRY']) . "</div>";
