98165f1029d96f80eef53d8aac7ad30f62a9b046
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-04 13:10:47 +0800

    linkedin share test

--- components/shared/share.php
+++ components/shared/share.php
@@ -15,7 +15,7 @@ onclick="
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
 <?php // LINKEDIN SHARE ?>
-<a href="https://www.linkedin.com/cws/share?url=http%3A%2F%2Fmjlh.mcgill.ca/blogs?blog_id=79" class="js-share">
+<a href="https://www.linkedin.com/cws/share?url=http%3A%2F%2Fmjlh.mcgill.ca/blog.php?blog_id=79" class="js-share">
   <i class="icon-linkedin-squared"></i>
 </a>
 
