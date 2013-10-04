f5b5868d7af2a561bcb0a9ce1c1adab276a67db3
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-04 19:02:23 +0800

    add search bar to homepage sidebar

---
+++ components/sidebar_cards/search.php
@@ -0,0 +1,15 @@
+<div class="card search-card">
+  <script>
+    (function() {
+      var cx = '011409353577078830325:y0f0pfr3hie';
+      var gcse = document.createElement('script');
+      gcse.type = 'text/javascript';
+      gcse.async = true;
+      gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+          '//www.google.com/cse/cse.js?cx=' + cx;
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(gcse, s);
+    })();
+  </script>
+  <gcse:search></gcse:search>
+</div>
