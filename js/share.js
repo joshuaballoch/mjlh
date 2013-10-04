12579fcf1a0f493615142473bf18150284ff6966
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-04 13:19:04 +0800

    fix twitter and linkedin shares

--- js/share.js
+++ js/share.js
@@ -8,5 +8,10 @@ $(document).on("click",".js-share",function(event){
   event.preventDefault();
   var $target = $(event.currentTarget);
   console.log($target.attr("href"));
-  window.open($target.attr("href"));
+  newPopup($target.attr("href"));
 });
+
+function newPopup(url) {
+  popupWindow = window.open(
+    url,'popUpWindow','height=300,width=600,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes');
+}
