d6183f08b199b42a12b0fd6e8d2b72ede198a61c
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-20 15:55:58 +0800

    fix post php refactor

--- components/blog/post.php
+++ components/blog/post.php
@@ -1,13 +1,28 @@
 <?php
-  echo "<div class=\"card\">";
   $read_more_link = "blog.php?blog_id=". $line['BLOG_ID'];
-  echo "<h2><a href =". $read_more_link .">" . $line['TITLE'] . "</a></h2><div class=\"author_stamp\">\n";
-  talk ("Posted By","Post&eacute; par",$lang);
-  echo " " . $line['AUTHOR'] . " <i>(" . $line['date'] . ")</i></div>\n";
-  echo "<div id = 'homeblogentry'>" . printTruncated(800,$line['ENTRY']) . "</div>";
 ?>
+
+<div class="card">
+  <h2>
+    <a href="<?php echo $read_more_link ?>">
+      <?php echo $line['TITLE'] ?>
+    </a>
+  </h2>
+  <div class="author_stamp">
+    <?php talk("Posted By", "Post&eacute; par", $lang); ?>
+    <?php echo " ".$line['AUTHOR']." "?>
+    <i>
+      <?php echo "(".$line['date'].")"?>
+    </i>
+  </div>
+  <div class = 'content'>
+    <?php
+      printTruncated(800,$line['ENTRY']);
+    ?>
+  </div>
+
   <a class="read_more" href="<?php echo $read_more_link ?>" >
     <i class="icon-right-circled"></i>
     <?php talk("Read more","En savoir plus",$lang); ?>
   </a>
-  </div>
+</div>
