cac2fe2577f7d5149da56277a4440501bc7815e6
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-20 17:12:15 +0800

    change nav wording

--- header.php
+++ header.php
@@ -89,8 +89,8 @@
           <nav>
             <ul class="nav">
               <?php $path = explode('?', $_SERVER['REQUEST_URI'], 2); $path = $path[0];?>
-              <li><a class="<?php if ($path === "/index.php" || $path === "/") echo "active" ;?>" href="index.php"><?php talk("Home","&nbsp;Accueil&nbsp;",$lang); ?></a></li>
-              <li><a class="<?php if ($path === "/volumes.php" || $path === "/articles.php") echo "active" ;?>" href="volumes.php"><?php talk("Issues","&nbsp;Numéros&nbsp;",$lang); ?></a></li>
+              <li><a class="<?php if ($path === "/index.php" || $path === "/") echo "active" ;?>" href="index.php"><?php talk("Online","En Ligne",$lang); ?></a></li>
+              <li><a class="<?php if ($path === "/volumes.php" || $path === "/articles.php") echo "active" ;?>" href="volumes.php"><?php talk("The Journal","Le Journal",$lang); ?></a></li>
               <li><a class="<?php if ($_SERVER["REQUEST_URI"] === "/page.php?id=colloquiumhome") echo "active" ;?>" href="page.php?id=colloquiumhome" rel = "colloquiumsubmenu"><?php talk("Colloquium","&nbsp;Colloque&nbsp;",$lang); ?></a></li>
               <li><a class="<?php if ($_SERVER["REQUEST_URI"] === "/page.php?id=submissions") echo "active" ;?>" href="page.php?id=submissions"><?php talk("Submissions","Soumissions",$lang); ?></a></li>
             </ul>
