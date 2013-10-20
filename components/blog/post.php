6816535a23fe109d22c1506906682fd8440cdc6e
Joshua Balloch <joshuaballoch@gmail.com>
2013-10-20 15:37:42 +0800

    visual improvement to cards

--- components/blog/post.php
+++ components/blog/post.php
@@ -1,12 +1,13 @@
 <?php
   echo "<div class=\"card\">";
   $read_more_link = "blog.php?blog_id=". $line['BLOG_ID'];
-  echo "<h2><a href =". $read_more_link .">" . $line['TITLE'] . "</a></h2><div style = \"padding-bottom: 5px; border-bottom: 1px dotted black;\">\n";
+  echo "<h2><a href =". $read_more_link .">" . $line['TITLE'] . "</a></h2><div class=\"author_stamp\">\n";
   talk ("Posted By","Post&eacute; par",$lang);
-  echo " <strong>" . $line['AUTHOR'] . "</strong> <i>(" . $line['date'] . ")</i></div>\n";
+  echo " " . $line['AUTHOR'] . " <i>(" . $line['date'] . ")</i></div>\n";
   echo "<div id = 'homeblogentry'>" . printTruncated(800,$line['ENTRY']) . "</div>";
 ?>
   <a class="read_more" href="<?php echo $read_more_link ?>" >
+    <i class="icon-right-circled"></i>
     <?php talk("Read more","En savoir plus",$lang); ?>
   </a>
   </div>
